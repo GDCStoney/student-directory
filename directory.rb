2ed4ba4b977021f2ccca602dfe13ae3d75171584
GDCStoney <g.stoney@btinternet.com>
2020-10-02 08:21:25 +0100

    Adds array and count functionality

--- directory.rb
+++ directory.rb
@@ -1,20 +1,23 @@
 # Setup variables
-student_count = 11
+students = [
+  'Dr. Hannibal Lecter',
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
-# first we print the list of students
+# print the list of students
 puts "\nThe students of Ciallains Academy"
 puts "--------------"
-puts 'Dr. Hannibal Lecter'
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-
-# Finally we print the total number of students
-print "Overall, we have #{student_count} great students\n\n"
+students.each do |student|
+  puts student
+end
+# print the total number of students
+print "\nOverall, we have #{students.count} great students\n\n"
