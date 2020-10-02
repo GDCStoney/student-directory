a533a0397285942993c3d26571e29034142c8be6
GDCStoney <g.stoney@btinternet.com>
2020-10-02 08:10:38 +0100

    Adds comments to the code for comprehension / progress

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "\nThe students of Ciallains Academy"
 puts "--------------"
 puts 'Dr. Hannibal Lecter'
@@ -12,6 +13,7 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
+# Finally we print the total number of students
 print "Overall, we have "
 print 11
 puts " great students\n\n"
